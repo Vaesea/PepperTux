7e3e37232580194b295c087da804e0acda1a266b
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-06-17 16:55:15 +0100

    something i forgot to do

--- scenes_and_scripts/levels/world3/level_1.gd
+++ scenes_and_scripts/levels/world3/level_1.gd
@@ -4,4 +4,5 @@ func _ready() -> void:
 	super._ready()
 	Global.world_1_unlocked = true
 	Global.world_2_unlocked = true
+	Global.world_3_unlocked = true
 	Global.save_data()
--- scenes_and_scripts/menu/main_menu.gd
+++ scenes_and_scripts/menu/main_menu.gd
@@ -20,8 +20,10 @@ func _process(_delta: float) -> void:
 	elif Input.is_key_pressed(KEY_2) and Global.world_2_unlocked == false:
 		OS.alert("You need to unlock World 2!", "Error")
 	
-	if Input.is_key_pressed(KEY_3):
-		OS.alert("World 3 has not been added yet.", "Error")
+	if Input.is_key_pressed(KEY_3) and Global.world_3_unlocked == true:
+		get_tree().change_scene_to_file("res://scenes_and_scripts/levels/world3/level1.tscn")
+	elif Input.is_key_pressed(KEY_3) and Global.world_3_unlocked == false:
+		OS.alert("You need to unlock World 3!", "Error")
 	
 	if Input.is_key_pressed(KEY_4):
 		OS.alert("World 4 has not been added yet.", "Error")
